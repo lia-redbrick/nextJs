1a5983909c648cd0b708021334eeaf54a62ee367
lia-redbrick <lia@redbrick.space>
2022-04-05 09:40:09 +0900

    1-3 Routing, add styles directly

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+/*
+    페이지 간  클라이언트 측 경로 전환을 활성화하고
+    single-page app 경험을 제공하려면 
+    Link Component가 필요
+*/
+
 export default function NavBar() {
+    const router = useRouter();
+
     return (
         <nav>
-            <a href="/Home">Home</a>
-            <a href="/about">About</a>
+            <Link href="/"><a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a></Link>
+            <Link href="/about"><a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>About</a></Link>
         </nav>
     )
 
